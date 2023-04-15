a5b19a0778d3a954e8cd6385184e5b90d9231701
deguet <joris.deguet@gmail.com>
2023-04-15 12:36:40 -0400

    fix url

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
     nom: "420-SN1 - Programmation en sciences",
     description: "Ceci est le site avec le contenu du cours de programmation pour les étudiants en pré-universitaire en sciences",
-    nomUrl: "sn1"
+    nomUrl: "420-SN1"
 }
 
 module.exports = config;
