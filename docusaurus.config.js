e9368c5ebdc58fa88d35924bcf3148e180dc4e13
Mathieu Dupont <mathieu.jean.dupont@gmail.com>
2025-08-15 13:08:37 -0400

    updates, exercices, katex

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,6 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
-import { themes } from 'prism-react-renderer';
+import {themes} from 'prism-react-renderer';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
@@ -11,137 +11,141 @@ const darkCodeTheme = themes.vsDark
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: siteConfig.nom,
-  tagline: siteConfig.description,
-  url: "https://info.cegepmontpetit.ca/",
-  baseUrl: `/${siteConfig.nomUrl}/`,
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+    title: siteConfig.nom,
+    tagline: siteConfig.description,
+    url: "https://info.cegepmontpetit.ca/",
+    baseUrl: `/${siteConfig.nomUrl}/`,
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/favicon.ico",
 
-  organizationName: "departement-info-cem",
-  projectName: siteConfig.nomUrl,
-  deploymentBranch: "gh-pages",
-  trailingSlash: false,
+    organizationName: "departement-info-cem",
+    projectName: siteConfig.nomUrl,
+    deploymentBranch: "gh-pages",
+    trailingSlash: false,
 
-  i18n: {
-    defaultLocale: "fr",
-    locales: ["fr"],
-  },
+    i18n: {
+        defaultLocale: "fr",
+        locales: ["fr"],
+    },
 
-  presets: [
-    [
-      "classic",
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          routeBasePath: "/",
-          editUrl: `https://github.com/departement-info-cem/${siteConfig.nomUrl}/tree/main`,
-          remarkPlugins: [remarkMath],
-          rehypePlugins: [rehypeKatex],
-        },
-        theme: {
-          customCss: [require.resolve("./src/css/custom.css")],
-        },
-        blog: {
-          remarkPlugins: [remarkMath],
-          rehypePlugins: [rehypeKatex],
-        },
-        pages: {
-          remarkPlugins: [remarkMath],
-          rehypePlugins: [rehypeKatex],
-        },
-      }),
+    presets: [
+        [
+            "classic",
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve("./sidebars.js"),
+                    routeBasePath: "/",
+                    editUrl: `https://github.com/departement-info-cem/${siteConfig.nomUrl}/tree/main`,
+                    remarkPlugins: [remarkMath],
+                    rehypePlugins: [rehypeKatex],
+                },
+                theme: {
+                    customCss: [require.resolve("./src/css/custom.css")],
+                },
+                blog: {
+                    remarkPlugins: [remarkMath],
+                    rehypePlugins: [rehypeKatex],
+                },
+                pages: {
+                    remarkPlugins: [remarkMath],
+                    rehypePlugins: [rehypeKatex],
+                },
+            }),
+        ],
     ],
-  ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      docs: {
-        sidebar: {
-          hideable: true,
-        },
-      },
-      navbar: {
-        title: siteConfig.nom,
-        logo: {
-          alt: "Logo CEM",
-          src: "img/logo.svg",
-        },
-        items: [
-          {
-            type: "doc",
-            docId: "cours/rencontre1",
-            position: "left",
-            label: "Cours",
-          },
-          {
-            type: "docSidebar",
-            position: "left",
-            sidebarId: "tp",
-            label: "Travaux Pratiques",
-          },
-          {
-            type: "docSidebar",
-            position: "left",
-            sidebarId: "recettes",
-            label: "Recettes",
-          },
-          {
-            type: "docSidebar",
-            position: "left",
-            sidebarId: "aidememoire",
-            label: "Aide-mémoire",
-          },
-        ],
-      },
-      footer: {
-        style: "dark",
-        links: [
-          {
-            title: "Sources",
-            items: [
-              {
-                label: "GitHub",
-                href: `https://github.com/departement-info-cem/${siteConfig.nomUrl}`,
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()}. ${
-          siteConfig.nom
-        }. CÉGEP Édouard-Montpetit.`,
-      },
-      // Décommenter et remplir pour activer l'indexation des pages par le moteur de recherche local
-      // algolia: {
-      //   appId: '',
-      //   apiKey: '',
-      //   indexName: '',
-      //   contextualSearch: true,
-      //   searchPagePath: 'search',
-      // },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-        additionalLanguages: ["csharp", "java", "python"],
-      },
-      metadata: [
+    stylesheets: [
         {
-          name: "keywords",
-          content: `${siteConfig.nom}, ${siteConfig.description}, informatique, technique, cégep, cegep, édouard-montpetit, edouard-montpetit, édouard montpetit, edouard montpetit`,
-        },
-      ],
-      stylesheets: [
-          {
-            href: 'https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css',
+            href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
             type: 'text/css',
-            integrity: 'sha384-mll67QQZFKtJkHv4dNUNz6EDuJeayQMZT6X0hgqP/d9vyvyehZ9UUuqbB6+L1tWg',
+            integrity:
+                'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
             crossorigin: 'anonymous',
-          },
-      ],
-    }),
+        },
+    ],
+
+    themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            docs: {
+                sidebar: {
+                    hideable: true,
+                },
+            },
+            navbar: {
+                title: siteConfig.nom,
+                logo: {
+                    alt: "Logo CEM",
+                    src: "img/logo.svg",
+                },
+                items: [
+                    {
+                        type: "doc",
+                        docId: "cours/rencontre1",
+                        position: "left",
+                        label: "Cours",
+                    },
+                    {
+                        type: "docSidebar",
+                        position: "left",
+                        sidebarId: "tp",
+                        label: "Travaux Pratiques",
+                    },
+                    {
+                        type: "docSidebar",
+                        position: "left",
+                        sidebarId: "recettes",
+                        label: "Recettes",
+                    },
+                    {
+                        type: "docSidebar",
+                        position: "left",
+                        sidebarId: "aidememoire",
+                        label: "Aide-mémoire",
+                    },
+                ],
+            },
+            footer: {
+                style: "dark",
+                links: [
+                    {
+                        title: "Sources",
+                        items: [
+                            {
+                                label: "GitHub",
+                                href: `https://github.com/departement-info-cem/${siteConfig.nomUrl}`,
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()}. ${
+                    siteConfig.nom
+                }. CÉGEP Édouard-Montpetit.`,
+            },
+            // Décommenter et remplir pour activer l'indexation des pages par le moteur de recherche local
+            // algolia: {
+            //   appId: '',
+            //   apiKey: '',
+            //   indexName: '',
+            //   contextualSearch: true,
+            //   searchPagePath: 'search',
+            // },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+                additionalLanguages: ["csharp", "java", "python"],
+            },
+            metadata: [
+                {
+                    name: "keywords",
+                    content: `${siteConfig.nom}, ${siteConfig.description}, informatique, technique, cégep, cegep, édouard-montpetit, edouard-montpetit, édouard montpetit, edouard montpetit`,
+                },
+            ],
+        }),
 };
 
 module.exports = config;
+
+
