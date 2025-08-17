f5c0402454371bb4d397c3d1bf232448a1b106ab
Mathieu Dupont <mathieu.jean.dupont@gmail.com>
2025-08-17 14:41:27 -0400

    code pour liens vers onglets

---
+++ src/theme/Tabs/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import OriginalTabs from '@theme-original/Tabs';
+
+export default function TabsWrapper(props) {
+  return <OriginalTabs queryString="onglet" {...props} />;
+}
