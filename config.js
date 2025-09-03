1f25523f2a1ddd32ed489f18bd881f2295924825
Mathieu Dupont <mathieu.jean.dupont@gmail.com>
2025-09-03 11:58:45 -0400

    URL corrigée

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
     nom: "420-SN1 - Programmation en sciences",
     description: "Site du cours 420-SN1 Programmation en sciences au cégep Édouard-Montpetit.",
-    nomUrl: "420-sn1"
+    nomUrl: "420-SN1"
 };
 
 module.exports = config;
