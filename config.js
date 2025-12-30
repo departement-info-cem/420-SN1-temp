75815fe44d0eea922917bdfad83738f2e69c99e3
Mathieu Dupont <mathieu.jean.dupont@gmail.com>
2025-12-30 17:12:20 -0500

    Change nomUrl from '420-SN1' to '420-SN1-temp'

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
     nom: "420-SN1 - Programmation en sciences",
     description: "Site du cours 420-SN1 Programmation en sciences au cégep Édouard-Montpetit.",
-    nomUrl: "420-SN1"
+    nomUrl: "420-SN1-temp"
 };
 
 module.exports = config;
